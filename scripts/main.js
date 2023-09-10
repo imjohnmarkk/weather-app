90957f496b1781677e6d3f6a8c6616062611d4ab
John Mark <john.mark.devv@gmail.com>
2023-09-10 19:33:15 +0800

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,59 @@
+const apiKey = "83ffce0510f2d7a34047ce7fa6bc9552";
+const apiURL =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+  const res = await fetch(apiURL + city + `&appid=${apiKey}`);
+  let data = await res.json();
+
+  console.log(data);
+
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temperature").innerHTML = `${Math.round(
+    data.main.temp
+  )}°C`;
+  document.querySelector(".humidity").innerHTML = `${data.main.humidity}%`;
+  document.querySelector(".wind").innerHTML = `${data.wind.speed}km/h`;
+
+  let weather = data.weather[0].main;
+
+  switch (weather) {
+    case "Clouds":
+      weatherIcon.src = "images/clouds.png";
+      break;
+
+    case "Clear":
+      weatherIcon.src = "images/clear.png";
+      break;
+
+    case "Drizzle":
+      weatherIcon.src = "images/drizzle.png";
+      break;
+
+    case "Mist":
+      weatherIcon.src = "images/mist.png";
+      break;
+
+    case "Rain":
+      weatherIcon.src = "images/rain.png";
+      break;
+
+    case "Snow":
+      weatherIcon.src = "images/snow.png";
+      break;
+
+    default:
+      weatherIcon.src = "images/clear.png";
+  }
+
+  document.querySelector(".weather").style.display = "block";
+}
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
