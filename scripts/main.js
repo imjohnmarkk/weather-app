e1575ba76ee4064206e00b2499383d1948d78674
John Mark <john.mark.devv@gmail.com>
2023-09-10 20:14:08 +0800

    Fixing stupid error

--- scripts/main.js
+++ scripts/main.js
@@ -57,6 +57,7 @@ async function checkWeather(city) {
     }
 
     document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
   }
 }
 
