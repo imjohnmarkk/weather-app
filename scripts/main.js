2d1e5cc5e5fdcb541809f4b2f04ba97d69657b6c
John Mark <john.mark.devv@gmail.com>
2023-09-10 20:01:10 +0800

    golong golo na bohay ko

--- scripts/main.js
+++ scripts/main.js
@@ -9,49 +9,55 @@ const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city) {
   const res = await fetch(apiURL + city + `&appid=${apiKey}`);
-  let data = await res.json();
 
-  console.log(data);
+  if (res.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    let data = await res.json();
 
-  document.querySelector(".city").innerHTML = data.name;
-  document.querySelector(".temperature").innerHTML = `${Math.round(
-    data.main.temp
-  )}°C`;
-  document.querySelector(".humidity").innerHTML = `${data.main.humidity}%`;
-  document.querySelector(".wind").innerHTML = `${data.wind.speed}km/h`;
+    console.log(data);
 
-  let weather = data.weather[0].main;
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temperature").innerHTML = `${Math.round(
+      data.main.temp
+    )}°C`;
+    document.querySelector(".humidity").innerHTML = `${data.main.humidity}%`;
+    document.querySelector(".wind").innerHTML = `${data.wind.speed}km/h`;
 
-  switch (weather) {
-    case "Clouds":
-      weatherIcon.src = "images/clouds.png";
-      break;
+    let weather = data.weather[0].main;
 
-    case "Clear":
-      weatherIcon.src = "images/clear.png";
-      break;
+    switch (weather) {
+      case "Clouds":
+        weatherIcon.src = "images/clouds.png";
+        break;
 
-    case "Drizzle":
-      weatherIcon.src = "images/drizzle.png";
-      break;
+      case "Clear":
+        weatherIcon.src = "images/clear.png";
+        break;
 
-    case "Mist":
-      weatherIcon.src = "images/mist.png";
-      break;
+      case "Drizzle":
+        weatherIcon.src = "images/drizzle.png";
+        break;
 
-    case "Rain":
-      weatherIcon.src = "images/rain.png";
-      break;
+      case "Mist":
+        weatherIcon.src = "images/mist.png";
+        break;
 
-    case "Snow":
-      weatherIcon.src = "images/snow.png";
-      break;
+      case "Rain":
+        weatherIcon.src = "images/rain.png";
+        break;
 
-    default:
-      weatherIcon.src = "images/clear.png";
+      case "Snow":
+        weatherIcon.src = "images/snow.png";
+        break;
+
+      default:
+        weatherIcon.src = "images/clear.png";
+    }
+
+    document.querySelector(".weather").style.display = "block";
   }
-
-  document.querySelector(".weather").style.display = "block";
 }
 
 searchBtn.addEventListener("click", () => {
